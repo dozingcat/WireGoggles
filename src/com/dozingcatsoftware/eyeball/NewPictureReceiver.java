a32ee505b06b2bbccd7d9b3380c1d94ec8414a91
Brian Nenninger <bnenning@gmail.com>
2017-09-09 15:09:41 -0400

    Add link to source on about page.

--- src/com/dozingcatsoftware/eyeball/NewPictureReceiver.java
+++ src/com/dozingcatsoftware/eyeball/NewPictureReceiver.java
@@ -3,11 +3,16 @@ package com.dozingcatsoftware.eyeball;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 public class NewPictureReceiver extends BroadcastReceiver {
 
     @Override public void onReceive(Context context, Intent intent) {
+        // On Android N and later we use a JobService and shouldn't get this notification.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return;
+        }
         Log.i("NewPictureReceiver", "Got picture: " + intent.getData());
         try {
             (new ProcessPictureOperation()).processPicture(context, intent.getData());
